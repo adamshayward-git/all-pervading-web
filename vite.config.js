0f97b537936b0d72d043769aef83ea1e271c6ee8
Adam Hayward <adam@wahagrill.com>
2026-03-11 08:54:53 -0700

    Configure Vite multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from "vite"
+import { resolve } from "path"
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        projects: "projects.html",
-        services: "services.html",
-        experiments: "experiments.html",
-        articles: "articles.html",
-        contact: "contact.html"
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "projects.html"),
+        services: resolve(__dirname, "services.html"),
+        experiments: resolve(__dirname, "experiments.html"),
+        articles: resolve(__dirname, "articles.html"),
+        contact: resolve(__dirname, "contact.html"),
+        thankYou: resolve(__dirname, "thank-you.html"),
+
+        restaurantWebsite: resolve(__dirname, "projects/restaurant-website.html"),
+        ecommerceStore: resolve(__dirname, "projects/ecommerce-store.html"),
+        webglInterface: resolve(__dirname, "projects/webgl-interface.html"),
+
+        seoForLocalBusiness: resolve(__dirname, "articles/seo-for-local-business.html"),
+        whyPerformanceMatters: resolve(__dirname, "articles/why-performance-matters.html")
       }
     }
   }
